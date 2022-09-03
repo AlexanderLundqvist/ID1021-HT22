c9943138dd1ebc5f83717a9d54f3137fda7fd56f
Alexander Lundqvist <33635814+KrimReaper@users.noreply.github.com>
2022-09-03 19:17:28 +0200

    Started task 3

---
+++ Tasks/Searching/src/searching/Searching.java
@@ -0,0 +1,17 @@
+
+package searching;
+
+/**
+ *
+ * @author Alexander
+ */
+public class Searching {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
