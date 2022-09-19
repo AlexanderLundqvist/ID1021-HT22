b76cc4074905782c05f1ba4cee2969b8107331ac
Alexander Lundqvist <33635814+KrimReaper@users.noreply.github.com>
2022-09-19 03:27:15 +0200

    List structure done

--- Tasks/Assignment5/src/assignment5/LinkedList.java
+++ Tasks/Assignment5/src/assignment5/LinkedList.java
@@ -35,6 +35,11 @@ public class LinkedList{
         return this.size;
     }
     
+    public boolean isEmpty() {
+        if(this.size == 0) return true;
+        else return false;
+    }
+    
     public void push(int value) {
         Node newHead = new Node(value, this.head);
         this.head = newHead;
@@ -42,15 +47,25 @@ public class LinkedList{
     }
     
     public Node pop() {
+        if(isEmpty()) {
+            System.out.println("The list is empty!");
+            return null;
+        }
         Node popped = head;
         this.head = head.next;
         size--;
         return popped;
     }
 
-    public void listToString() {
-        while() {
-            
+    public void print() {
+        if(isEmpty()) System.out.println("The list is empty!");
+        else {
+            System.out.println(head.getValue() + " <- Top of the stack");
+            Node current = head.next;
+            while(current != null) {
+                System.out.println(current.getValue());
+                current = current.next;
+            }
         }
     }
     
@@ -60,6 +75,29 @@ public class LinkedList{
      * @param args takes no input arguments
      */    
     public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        
+        list.print();
+        System.out.println();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        list.push(4);
+        list.push(5);
+        list.print();
+        System.out.println();
+        list.pop();
+        
+        list.print();
+        System.out.println();
+        list.pop();
+        list.pop();
+        list.pop();
+        list.pop();
+        System.out.println();
+        list.pop();
+        System.out.println();
+        list.print();
         
     }
 }
