14af1181e064d7a224b8d03b4df547eb7f0ef1b9
Alexander Lundqvist <33635814+KrimReaper@users.noreply.github.com>
2022-09-27 20:06:22 +0200

    Added Binary search (With integer objects) for comparison

---
+++ Tasks/Trees/src/trees/BinarySearch.java
@@ -0,0 +1,42 @@
+package trees;
+
+import java.util.*;
+
+public class BinarySearch {
+    
+    // Default constructor
+    public BinarySearch() {
+    }
+    
+    public static boolean search(Integer[] array, Integer key) {
+        int lo = 0; 
+        int hi = array.length - 1;
+        while (lo <= hi) {
+            int mid = lo + ((hi - lo) / 2);
+            if (array[mid] == key) return true;
+            else if (array[mid] > key && mid > lo) hi = mid - 1;
+            else if (array[mid] < key && mid < hi) lo = mid + 1;
+            else return false;
+        }
+        return false;
+    }
+    
+    public static long benchmark(Integer[] array, int iterations) {
+        Random rnd = new Random();
+        long t_total = 0;
+        for (int i = 0; i < iterations; i++) {
+            long t_start = System.nanoTime();
+            search(array, rnd.nextInt(array.length - 1));
+            t_total += System.nanoTime() - t_start;
+        }
+        return (t_total/iterations);
+    }    
+
+    /**
+     * Main method with unit testing for the class.
+     * @param args takes no input arguments
+     */    
+    public static void main(String[] args) {
+        
+    }
+}
